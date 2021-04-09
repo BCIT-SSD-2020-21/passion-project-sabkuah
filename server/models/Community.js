2ea8799b4497af8dde748e7de28c780890001d92
Russell Telen <rtelen@my.bcit.ca>
2021-04-08 18:49:17 -0700

    Initial definition of Community schema #4

---
+++ server/models/Community.js
@@ -0,0 +1,47 @@
+//==========================================
+// REQUIRE
+//==========================================
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+const User = require("./User")
+const Post = require("./Post")
+
+//==========================================
+// SCHEMA
+//==========================================
+const communitySchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  location: {
+    type: String,
+  },
+  contents: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Post",
+    },
+  ],
+  members: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+})
+
+//==========================================
+// SET UP MODEL
+//==========================================
+const Community = mongoose.model("Community", communitySchema)
+
+module.exports = Community
