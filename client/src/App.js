a46bcc88ffe8ee6ced49bcc72326da27d8b24986
Sabrina Kuah <swkuah@gmail.com>
2021-04-09 11:02:18 -0700

    Add default DrawerNav, #32

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import Register from './components/register/register';
 import SearchInput from './components/SearchInput';
+import DrawerNav from './components/DrawerNav';
 
 function App() {
   return (
     <div>
+      {/* <DrawerNav /> */}
       <Register />
       <SearchInput />
     </div>
