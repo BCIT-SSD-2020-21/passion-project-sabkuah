1bac1b1df3649abc5d82010e705cf1660ca8a82c
Kalvin Tang <ktang82@my.bcit.ca>
2021-04-10 15:43:54 -0700

    #14 create login page

--- client/src/App.js
+++ client/src/App.js
@@ -5,31 +5,32 @@ import User from './layouts/User';
 import Landing from './views/landing/Landing';
 import Dashboard from './views/dashboard/Dashboard';
 import Register from './views/register/Register';
+import Login from './views/login/Login';
 
 function App() {
-  return (
-    <Router>
-      <Switch>
-        <Route path='/user/:path?'>
-          <User>
+    return (
+        <Router>
             <Switch>
-              <Route path='/user' component={Dashboard} />
-            </Switch>
-          </User>
-        </Route>
+                <Route path="/user/:path?">
+                    <User>
+                        <Switch>
+                            <Route path="/user" component={Dashboard} />
+                        </Switch>
+                    </User>
+                </Route>
 
-        <Route path='/:path?'>
-          <Public>
-            <Switch>
-              <Route path='/register' component={Register} />
-              {/* <Route path='/login' component={Login} /> */}
-              <Route path='/' exact component={Landing} />
+                <Route path="/:path?">
+                    <Public>
+                        <Switch>
+                            <Route path="/register" component={Register} />
+                            <Route path="/login" component={Login} />
+                            <Route path="/" exact component={Landing} />
+                        </Switch>
+                    </Public>
+                </Route>
             </Switch>
-          </Public>
-        </Route>
-      </Switch>
-    </Router>
-  );
+        </Router>
+    );
 }
 
 export default App;
---
+++ client/src/views/login/LoginScreen.js
@@ -0,0 +1,99 @@
+import React, { Component } from 'react';
+import {
+    makeStyles,
+    TextField,
+    Button,
+    Card,
+    Typography,
+} from '@material-ui/core';
+import LockIcon from '@material-ui/icons/Lock';
+import EmailIcon from '@material-ui/icons/Email';
+
+const useStyles = makeStyles((theme) => ({
+    registerForm: {
+        display: 'flex',
+        flexDirection: 'column',
+        width: '90',
+        marginBottom: 20,
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 10,
+    },
+    input: {
+        width: '80%',
+        marginBottom: 20,
+    },
+    title: {
+        color: '#1cc47d',
+        marginBottom: '2%',
+    },
+    button: {
+        marginTop: '2%',
+        backgroundColor: '#192935',
+        color: '#fff',
+        padding: 10,
+        width: '50%',
+    },
+}));
+const Login = () => {
+    const classes = useStyles();
+    return (
+        <div>
+            <Card>
+                <form className={classes.registerForm}>
+                    <BlockwatchLogo />
+                    <Typography className={classes.title} variant="h3">
+                        Blockwatch
+                    </Typography>
+                    <text style={{ marginBottom: '3%' }}>
+                        Building safe communities
+                    </text>
+
+                    <TextField
+                        required
+                        variant="outlined"
+                        label="Email"
+                        placeholder="Email"
+                        className={classes.input}
+                        InputProps={{
+                            startAdornment: (
+                                <InputAdornment position="start">
+                                    <EmailIcon />
+                                </InputAdornment>
+                            ),
+                        }}
+                    />
+
+                    <TextField
+                        required={true}
+                        label="password"
+                        variant="outlined"
+                        placeholder="password"
+                        className={classes.input}
+                        type="password"
+                        id="password"
+                        InputProps={{
+                            startAdornment: (
+                                <InputAdornment position="start">
+                                    <LockIcon />
+                                </InputAdornment>
+                            ),
+                        }}
+                    />
+                    <Button
+                        variant="contained"
+                        type="submit"
+                        id="Register"
+                        className={classes.button}
+                    >
+                        Login
+                    </Button>
+
+                    <h4>Sign Up here</h4>
+                </form>
+            </Card>
+        </div>
+    );
+};
+
+export default Login;
