2b60aa3b22be122732e7a558fc709d4144284e7b
Russell Telen <rtelen@my.bcit.ca>
2021-04-12 16:28:11 -0700

    GET community postings #8

--- server/controller/posts.js
+++ server/controller/posts.js
@@ -4,3 +4,29 @@
 const User = require("../models/User")
 const Community = require("../models/Community")
 const Post = require("../models/Post")
+
+/**
+ * Retrieve all posts in a community
+ * @function
+ * @GET
+ * @param req.params :id - community id
+ * @returns {Array} List of posts in a community
+ * @throws Will throw an error if community not found
+ */
+module.exports.getCommunityPosts = async (req, res) => {
+  try {
+    const { id } = req.params
+    const community = await Community.findById(id)
+
+    const posts = await Post.find({ community: community._id })
+      .select("-community")
+      .populate({
+        path: "author",
+        select: "firstName lastName email location",
+      })
+
+    res.send({ posts })
+  } catch (e) {
+    console.log(e)
+  }
+}
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -3,9 +3,11 @@
 // =============================================
 const express = require("express")
 const router = express.Router({ mergeParams: true })
+const { getCommunityPosts } = require("../controller/posts")
 
 // ==============================================
 // ROUTES
 // ==============================================
+router.route("/").get(getCommunityPosts)
 
 module.exports = router
