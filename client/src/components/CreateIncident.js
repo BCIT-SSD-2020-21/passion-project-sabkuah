570ba5e92f2147c5663e2a2b0b4a98101043e555
Kalvin Tang <ktang82@my.bcit.ca>
2021-04-13 15:02:50 -0700

    add handlePost function #64

--- client/src/components/CreateIncident.js
+++ client/src/components/CreateIncident.js
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { TextField } from "@material-ui/core";
 import Modal from "react-bootstrap/Modal";
 import InputAdornment from "@material-ui/core/InputAdornment";
 import CreateIcon from "@material-ui/icons/Create";
-import addIncident from "../network/community";
+import { addIncident } from "../network/community";
 import useLocalStorage from "react-use-localstorage";
 
 const CreateIncident = ({ show, setShow }) => {
@@ -17,6 +17,28 @@ const CreateIncident = ({ show, setShow }) => {
     const handleClose = () => {
         setShow(false);
     };
+
+    const handlePost = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await addIncident(incident, token);
+            console.log(token);
+            console.log(incident);
+            if (response.error) {
+                console.log(response.error);
+                alert(response.error);
+                return;
+            }
+            if (response.token) {
+                setToken(response.token);
+                console.log("Post successful", response.data);
+                alert(response.message);
+                handleClose();
+            }
+        } catch (e) {
+            console.log("Error Posting Incident", e);
+        }
+    };
     return (
         <div>
             <Modal
@@ -29,7 +51,7 @@ const CreateIncident = ({ show, setShow }) => {
                 <h2 className="modal-title">New Incident</h2>
 
                 <div className="modal-body">
-                    <form className="modal-form">
+                    <form className="modal-form" onSubmit={handlePost}>
                         <TextField
                             variant="outlined"
                             label="Title"
@@ -61,20 +83,22 @@ const CreateIncident = ({ show, setShow }) => {
                         <TextField
                             variant="outlined"
                             label="Description"
+                            placeholder="Description"
+                            id="Description"
                             multiline={true}
                             id="email"
                             className="modal-form-input"
                             rows={5}
                         />
+
+                        <Modal.Footer>
+                            <button className="modal-btn" onClick={handleClose}>
+                                Close
+                            </button>
+                            <button className="modal-btn">Post</button>
+                        </Modal.Footer>
                     </form>
                 </div>
-
-                <Modal.Footer>
-                    <button className="modal-btn" onClick={handleClose}>
-                        Close
-                    </button>
-                    <button className="modal-btn">Submit</button>
-                </Modal.Footer>
             </Modal>
         </div>
     );
