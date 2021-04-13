7ccdca84b14a52b1d1c9f1dca1b26ad26cb1734f
Russell Telen <rtelen@my.bcit.ca>
2021-04-13 11:44:19 -0700

    Define Geometry schema #59

---
+++ server/models/Geometry.js
@@ -0,0 +1,27 @@
+//==========================================
+// REQUIRE
+//==========================================
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+
+//==========================================
+// SCHEMA
+//==========================================
+const geometrySchema = new Schema({
+  type: {
+    type: String,
+    enum: ["Point"],
+    required: true,
+  },
+  coordinates: {
+    type: [Number],
+    required: true,
+  },
+})
+
+//==========================================
+// SET UP MODEL
+//==========================================
+const Geometry = mongoose.model("Geometry", geometrySchema)
+
+module.exports = Geometry
