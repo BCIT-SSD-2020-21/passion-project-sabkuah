5ad92eeb4b39fb70ddeab98f63f6c7ce3521a608
Sabrina Kuah <swkuah@gmail.com>
2021-04-13 00:04:17 -0700

    Add PrivateRoute wrapper to App.js, #28

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,11 @@
+import { useEffect, useState } from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from 'react-router-dom';
 import Public from './layouts/Public';
 import User from './layouts/User';
 import Landing from './views/landing/Landing';
@@ -7,35 +13,52 @@ import Register from './views/register/Register';
 import Login from './views/login/Login';
 import Search from './views/search/Search';
 import UserCommunities from './views/userCommunities/UserCommunities';
+import jwtDecode from 'jwt-decode';
+import useLocalStorage from 'react-use-localstorage';
 
 function App() {
-    return (
-        <Router>
-            <Switch>
-                <Route path="/user/:path?">
-                    <User>
-                        <Switch>
-                            <Route
-                                path="/user/communities"
-                                component={UserCommunities}
-                            />
-                        </Switch>
-                    </User>
-                </Route>
+  const [token] = useLocalStorage('token');
+  const [user, setUser] = useState();
 
-                <Route path="/:path?">
-                    <Public>
-                        <Switch>
-                            <Route path="/register" component={Register} />
-                            <Route path="/search" component={Search} />
-                            <Route path="/login" component={Login} />
-                            <Route path="/" exact component={Landing} />
-                        </Switch>
-                    </Public>
-                </Route>
+  useEffect(() => {
+    if (!token) {
+      setUser(null);
+      return;
+    }
+    const userData = jwtDecode(token);
+    setUser(userData);
+  }, [token]);
+
+  const PrivateRoute = ({ path, children }) => (
+    <Route path={path}>{!!user ? children : <Redirect to='/login' />}</Route>
+  );
+
+  return (
+    <Router>
+      <Switch>
+        <Route path='/user/:path?'>
+          <User>
+            <Switch>
+              <PrivateRoute path='/user/communities'>
+                <UserCommunities />
+              </PrivateRoute>
             </Switch>
-        </Router>
-    );
+          </User>
+        </Route>
+
+        <Route path='/:path?'>
+          <Public>
+            <Switch>
+              <Route path='/register' component={Register} />
+              <Route path='/search' component={Search} />
+              <Route path='/login' component={Login} />
+              <Route path='/' exact component={Landing} />
+            </Switch>
+          </Public>
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
