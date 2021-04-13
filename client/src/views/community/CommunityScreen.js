09ca7e44d27e1a220bd4eab2958da26a38221e13
Sabrina Kuah <swkuah@gmail.com>
2021-04-13 15:24:30 -0700

    Fix modal close when submitted

--- client/src/views/community/CommunityScreen.js
+++ client/src/views/community/CommunityScreen.js
@@ -5,6 +5,7 @@ const CommunityScreen = ({ community }) => {
     <div>
       <h1>{community?.title}</h1>
       <p>Description: {community?.description}</p>
+      <p>Location: {community?.location}</p>
     </div>
   );
 };
