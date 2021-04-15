7dd3ac4a040d8a2073c62d95dd60b4cb32c6bc70
Sabrina Kuah <swkuah@gmail.com>
2021-04-15 15:35:24 -0700

    Delete icon renders conditionally for author, #87

--- client/src/components/Comment.js
+++ client/src/components/Comment.js
@@ -1,28 +1,42 @@
 import { Avatar, IconButton } from '@material-ui/core';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import CancelIcon from '@material-ui/icons/Cancel';
+import useLocalStorage from 'react-use-localstorage';
+import jwtDecode from 'jwt-decode';
 
 const Comment = ({ comment, handleDeleteComment, postId }) => {
+  const [token] = useLocalStorage('token');
+  const [currentUser, setCurrentUser] = useState(null);
+
+  useEffect(() => {
+    const user = jwtDecode(token);
+    setCurrentUser(user);
+  }, []);
+
   return (
     <div
       className='w-100 ml-4 my-3 p-1'
       style={{ backgroundColor: 'lightgray', borderRadius: '1rem' }}
     >
-      <div className='row'>
-        <Avatar src={comment.author.avatar} className='mr-3' />
-        <div className='mx-3'>
-          <strong className='row'>
-            {comment.author.firstName} {comment.author.lastName}
-          </strong>
-          {comment.body}
+      <div className='row justify-content-between'>
+        <div className='row'>
+          <Avatar src={comment.author.avatar} className='mr-3' />
+          <div className='mx-3'>
+            <strong className='row'>
+              {comment.author.firstName} {comment.author.lastName}
+            </strong>
+            {comment.body}
+          </div>
         </div>
-        <IconButton
-          onClick={() => {
-            handleDeleteComment(postId, comment._id);
-          }}
-        >
-          <CancelIcon />
-        </IconButton>
+        {currentUser?._id === comment.author._id && (
+          <IconButton
+            onClick={() => {
+              handleDeleteComment(postId, comment._id);
+            }}
+          >
+            <CancelIcon />
+          </IconButton>
+        )}
       </div>
     </div>
   );
