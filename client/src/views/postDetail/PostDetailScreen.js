32594ab50acd7996a46c9a0aa15f91bea1e1db91
Sabrina Kuah <swkuah@gmail.com>
2021-04-15 00:30:17 -0700

    Post comments rendered to PostDetails pg, #87

--- client/src/views/postDetail/PostDetailScreen.js
+++ client/src/views/postDetail/PostDetailScreen.js
@@ -1,7 +1,21 @@
+import { Card } from '@material-ui/core';
 import React from 'react';
 
-const PostDetailScreen = () => {
-  return <div>post details</div>;
+const PostDetailScreen = ({ post }) => {
+  return (
+    <div>
+      <h2>These are the comments for this post:</h2>
+      {post?.comments.map((comment) => (
+        <Card
+          key={comment._id}
+          className='m-3 p-4'
+          style={{ backgroundColor: 'grey' }}
+        >
+          {comment.body}
+        </Card>
+      ))}
+    </div>
+  );
 };
 
 export default PostDetailScreen;
