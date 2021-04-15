4d9576deedcb5637474d70aa704110b899b012f2
Kalvin Tang <ktang82@my.bcit.ca>
2021-04-14 22:41:38 -0700

    Add Textfield #98

--- client/src/components/EditPostModal.js
+++ client/src/components/EditPostModal.js
@@ -3,57 +3,57 @@ import Modal from 'react-bootstrap/Modal';
 import { TextField } from '@material-ui/core';
 import InputAdornment from '@material-ui/core/InputAdornment';
 import LocationCityIcon from '@material-ui/icons/LocationCity';
-// import Select from '@material-ui/core/Select';
-// import InputLabel from '@material-ui/core/InputLabel';
-// import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import InputLabel from '@material-ui/core/InputLabel';
+import FormControl from '@material-ui/core/FormControl';
 
 const EditPostModal = ({
-  showEditModal,
-  setShowEditModal,
-  post,
-  handleEdit,
+    showEditModal,
+    setShowEditModal,
+    post,
+    handleEdit,
 }) => {
-  const [editedPost, setEditedPost] = useState(post);
+    const [editedPost, setEditedPost] = useState(post);
 
-  const handleUpdatePost = async (e) => {
-    e.preventDefault();
-    await handleEdit(editedPost);
-    setShowEditModal(false);
-  };
+    const handleUpdatePost = async (e) => {
+        e.preventDefault();
+        await handleEdit(editedPost);
+        setShowEditModal(false);
+    };
 
-  return (
-    <Modal
-      show={showEditModal}
-      backdrop='static'
-      keyboard={false}
-      style={{ marginTop: '5%' }}
-    >
-      <h2 className='modal-title'>Update Post</h2>
-      <div className='modal-body'>
-        <form className='modal-form' onSubmit={handleUpdatePost}>
-          <TextField
-            required
-            value={editedPost.title}
-            variant='outlined'
-            label='Title'
-            placeholder='Title'
-            id='Title'
-            className='modal-form-input'
-            InputProps={{
-              startAdornment: (
-                <InputAdornment position='start'>
-                  <LocationCityIcon />
-                </InputAdornment>
-              ),
-            }}
-            onChange={(e) =>
-              setEditedPost({
-                ...editedPost,
-                title: e.target.value,
-              })
-            }
-          />
-          <TextField
+    return (
+        <Modal
+            show={showEditModal}
+            backdrop="static"
+            keyboard={false}
+            style={{ marginTop: '5%' }}
+        >
+            <h2 className="modal-title">Update Post</h2>
+            <div className="modal-body">
+                <form className="modal-form" onSubmit={handleUpdatePost}>
+                    <TextField
+                        required
+                        value={editedPost.title}
+                        variant="outlined"
+                        label="Title"
+                        placeholder="Title"
+                        id="Title"
+                        className="modal-form-input"
+                        InputProps={{
+                            startAdornment: (
+                                <InputAdornment position="start">
+                                    <LocationCityIcon />
+                                </InputAdornment>
+                            ),
+                        }}
+                        onChange={(e) =>
+                            setEditedPost({
+                                ...editedPost,
+                                title: e.target.value,
+                            })
+                        }
+                    />
+                    {/* <TextField
             required
             value={editedPost.category}
             variant='outlined'
@@ -74,8 +74,8 @@ const EditPostModal = ({
                 category: e.target.value,
               })
             }
-          />
-          {/* <FormControl>
+          /> */}
+                    <FormControl>
                         <InputLabel htmlFor="grouped-native-select">
                             Category
                         </InputLabel>
@@ -93,39 +93,39 @@ const EditPostModal = ({
 
                             <option value={3}>Option 3</option>
                         </Select>
-                    </FormControl> */}
+                    </FormControl>
 
-          <TextField
-            required
-            value={editedPost.description}
-            variant='outlined'
-            label='Description'
-            multiline={true}
-            id='email'
-            className='modal-form-input'
-            rows={5}
-            onChange={(e) =>
-              setEditedPost({
-                ...editedPost,
-                description: e.target.value,
-              })
-            }
-          />
-          <Modal.Footer>
-            <button
-              className='modal-btn'
-              onClick={() => setShowEditModal(false)}
-            >
-              Close
-            </button>
-            <button type='submit' className='modal-btn'>
-              Update
-            </button>
-          </Modal.Footer>
-        </form>
-      </div>
-    </Modal>
-  );
+                    <TextField
+                        required
+                        value={editedPost.description}
+                        variant="outlined"
+                        label="Description"
+                        multiline={true}
+                        id="email"
+                        className="modal-form-input"
+                        rows={5}
+                        onChange={(e) =>
+                            setEditedPost({
+                                ...editedPost,
+                                description: e.target.value,
+                            })
+                        }
+                    />
+                    <Modal.Footer>
+                        <button
+                            className="modal-btn"
+                            onClick={() => setShowEditModal(false)}
+                        >
+                            Close
+                        </button>
+                        <button type="submit" className="modal-btn">
+                            Update
+                        </button>
+                    </Modal.Footer>
+                </form>
+            </div>
+        </Modal>
+    );
 };
 
 export default EditPostModal;
