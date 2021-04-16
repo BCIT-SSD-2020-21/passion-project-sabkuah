5af797dc285e2718683b3fe8da7d5c403e299a0b
Sabrina Kuah <swkuah@gmail.com>
2021-04-16 01:09:31 -0700

    Fetch community data within sidebar, #114

--- client/src/views/dashboard/Dashboard.js
+++ client/src/views/dashboard/Dashboard.js
@@ -1,13 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Calendar from 'react-calendar';
 import Messaging from '../../components/Messaging';
 import Weather from '../../components/Weather';
 import { useLocation } from 'react-router-dom';
+import useLocalStorage from 'react-use-localstorage';
+import { getCommunity } from '../../network/community';
 
 const Dashboard = ({ children }) => {
   const [value, onChange] = useState(new Date());
   const location = useLocation();
   const USER_COMMUNITY_URL = '/user/communities';
+  const [token] = useLocalStorage('token');
+  const [community, setCommunity] = useState(null);
+
+  useEffect(() => {
+    console.log('useLocation>>', location);
+    const communityId = location.pathname.substr(18, 35);
+    console.log('SUBSTRING>>>>>>>', communityId);
+    communityId &&
+      (async () => {
+        const response = await getCommunity({ id: communityId, token });
+        console.log('community response', response.community);
+        setCommunity(response.community);
+      })();
+  }, []);
 
   return (
     <div>
@@ -16,13 +32,14 @@ const Dashboard = ({ children }) => {
           {children}
         </div>
 
-        <div
-          className='col-xs-12 col-lg-3 col-xs-12 col-lg-3 animate__animated animate__fadeInRight'
-          // style={{ backgroundColor: 'lightgrey' }}
-        >
-          {location.pathname !== USER_COMMUNITY_URL && <Weather />}
+        <div className='col-xs-12 col-lg-3 col-xs-12 col-lg-3 animate__animated animate__fadeInRight'>
+          {location.pathname !== USER_COMMUNITY_URL && (
+            <Weather community={community} />
+          )}
           <Calendar onChange={onChange} value={value} />
-          {location.pathname !== USER_COMMUNITY_URL && <Messaging />}
+          {location.pathname !== USER_COMMUNITY_URL && (
+            <Messaging community={community} />
+          )}
         </div>
       </div>
     </div>
