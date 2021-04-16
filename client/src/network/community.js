50c277b69db2b7ca924f7ca04b354fcb1ca97e35
Russell Telen <rtelen@my.bcit.ca>
2021-04-16 00:34:20 -0700

    Write edit community network call #115

--- client/src/network/community.js
+++ client/src/network/community.js
@@ -1,188 +1,205 @@
-import axios from 'axios';
+import axios from "axios"
 
-const BASE_URL = 'https://block-watch.herokuapp.com/api';
+const BASE_URL = "https://block-watch.herokuapp.com/api"
 
 export async function getAllCommunities() {
   try {
     const response = await axios({
-      method: 'GET',
+      method: "GET",
       url: `${BASE_URL}/communities`,
       headers: {
-        'Access-Control-Allow-Origin': '*',
+        "Access-Control-Allow-Origin": "*",
       },
-    });
-    return response.data;
+    })
+    return response.data
   } catch (e) {
-    console.log('Error:', e);
+    console.log("Error:", e)
   }
 }
 
 export async function addPost(post, token, id) {
   try {
     const response = await axios({
-      method: 'POST',
+      method: "POST",
       url: `${BASE_URL}/communities/${id}/posts`,
       data: post,
       headers: {
-        'Access-Control-Allow-Origin': '*',
+        "Access-Control-Allow-Origin": "*",
         Authorization: `Bearer ${token}`,
       },
-    });
-    console.log(response);
+    })
+    console.log(response)
 
-    return response.data;
+    return response.data
   } catch (e) {
-    console.log('Error', e);
+    console.log("Error", e)
   }
 }
 
 export async function getPosts(token, id) {
   try {
     const response = await axios({
-      method: 'GET',
+      method: "GET",
       url: `${BASE_URL}/communities/${id}/posts`,
       headers: {
-        'Access-Control-Allow-Origin': '*',
+        "Access-Control-Allow-Origin": "*",
         Authorization: `Bearer ${token}`,
       },
-    });
-    return response.data.posts;
+    })
+    return response.data.posts
   } catch (e) {
-    console.log('Error:', e);
+    console.log("Error:", e)
   }
 }
 
 export async function editPost(token, id, postId, post) {
-  console.log('edit called!');
+  console.log("edit called!")
   try {
     const response = await axios({
-      method: 'PATCH',
+      method: "PATCH",
       url: `${BASE_URL}/communities/${id}/posts/${postId}`,
       data: post,
       headers: {
-        'Access-Control-Allow-Origin': '*',
+        "Access-Control-Allow-Origin": "*",
         Authorization: `Bearer ${token}`,
       },
-    });
-    console.log('Edit posts response data>>> ', response.data);
-    return response.data;
+    })
+    console.log("Edit posts response data>>> ", response.data)
+    return response.data
   } catch (e) {
-    console.log('Error:', e);
+    console.log("Error:", e)
   }
 }
 
 export async function addCommunity(community, token) {
   try {
     const response = await axios({
-      method: 'POST',
+      method: "POST",
       url: `${BASE_URL}/communities`,
       data: community,
       headers: {
-        'Access-Control-Allow-Origin': '*',
+        "Access-Control-Allow-Origin": "*",
         Authorization: `Bearer ${token}`,
       },
-    });
-    return response.data;
+    })
+    return response.data
   } catch (e) {
-    console.log('Error', e);
+    console.log("Error", e)
+  }
+}
+
+export async function editCommunity(community, token, communityId) {
+  try {
+    const response = await axios({
+      method: "PATCH",
+      url: `${BASE_URL}/communities/${communityId}`,
+      data: community,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        Authorization: `Bearer ${token}`,
+      },
+    })
+    return response.data
+  } catch (e) {
+    console.log("Error", e)
   }
 }
 
 export async function getUserCommunities(token) {
   try {
     const response = await axios({
-      method: 'GET',
+      method: "GET",
       url: `${BASE_URL}/users/communities`,
       headers: {
-        'Access-Control-Allow-Origin': '*',
+        "Access-Control-Allow-Origin": "*",
         Authorization: `Bearer ${token}`,
       },
-    });
-    return response.data.user.communities;
+    })
+    return response.data.user.communities
   } catch (e) {
-    console.log('Error:', e);
+    console.log("Error:", e)
   }
 }
 
 export async function getCommunity({ id, token }) {
   try {
     const response = await axios({
-      method: 'GET',
+      method: "GET",
       url: `${BASE_URL}/communities/${id}`,
       headers: {
-        'Access-Control-Allow-Origin': '*',
+        "Access-Control-Allow-Origin": "*",
         Authorization: `Bearer ${token}`,
       },
-    });
-    return response.data;
+    })
+    return response.data
   } catch (e) {
-    console.log('Error:', e);
+    console.log("Error:", e)
   }
 }
 
 export async function joinCommunity({ id, token }) {
   try {
     const response = await axios({
-      method: 'PATCH',
+      method: "PATCH",
       url: `${BASE_URL}/communities/${id}/join`,
       headers: {
-        'Access-Control-Allow-Origin': '*',
+        "Access-Control-Allow-Origin": "*",
         Authorization: `Bearer ${token}`,
       },
-    });
-    return response.data;
+    })
+    return response.data
   } catch (e) {
-    console.log('Error:', e);
+    console.log("Error:", e)
   }
 }
 
 export async function getCommentsByPostId({ postId, token }) {
   try {
     const response = await axios({
-      method: 'GET',
+      method: "GET",
       url: `${BASE_URL}/posts/${postId}/comments`,
       headers: {
-        'Access-Control-Allow-Origin': '*',
+        "Access-Control-Allow-Origin": "*",
         Authorization: `Bearer ${token}`,
       },
-    });
-    return response.data;
+    })
+    return response.data
   } catch (e) {
-    console.log('Error:', e);
+    console.log("Error:", e)
   }
 }
 
 export async function addNewComment({ postId, token, comment }) {
-  const objComment = { body: comment };
+  const objComment = { body: comment }
   try {
     const response = await axios({
-      method: 'POST',
+      method: "POST",
       url: `${BASE_URL}/posts/${postId}/comments`,
       data: objComment,
       headers: {
-        'Access-Control-Allow-Origin': '*',
+        "Access-Control-Allow-Origin": "*",
         Authorization: `Bearer ${token}`,
       },
-    });
-    return response.data;
+    })
+    return response.data
   } catch (e) {
-    console.log('Error', e);
+    console.log("Error", e)
   }
 }
 
 export async function deleteComment({ postId, token, commentId }) {
-  console.log('postId>>', postId, 'commentId>>', commentId);
+  console.log("postId>>", postId, "commentId>>", commentId)
   try {
     const response = await axios({
-      method: 'DELETE',
+      method: "DELETE",
       url: `${BASE_URL}/posts/${postId}/comments/${commentId}`,
       headers: {
-        'Access-Control-Allow-Origin': '*',
+        "Access-Control-Allow-Origin": "*",
         Authorization: `Bearer ${token}`,
       },
-    });
-    return response.data;
+    })
+    return response.data
   } catch (e) {
-    console.log('Error', e);
+    console.log("Error", e)
   }
 }
