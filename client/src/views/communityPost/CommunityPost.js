ca7ae4a36a3ef732ef7b524920b561a160b02c37
Russell Telen <rtelen@my.bcit.ca>
2021-04-15 23:09:15 -0700

    Include widgets in CommunityPost #92

--- client/src/views/communityPost/CommunityPost.js
+++ client/src/views/communityPost/CommunityPost.js
@@ -4,12 +4,16 @@ import useLocalStorage from "react-use-localstorage"
 import { useParams } from "react-router-dom"
 import { getPosts, editPost } from "../../network/community"
 import toastr from "toastr"
+import Calendar from "react-calendar"
+import Messaging from "../../components/Messaging"
+import Weather from "../../components/Weather"
 
 const CommunityPosts = () => {
   const [posts, setPosts] = useState(null)
   let { id } = useParams()
   const [token] = useLocalStorage("token")
   const [refreshEdit, setRefreshEdit] = useState("")
+  const [value, onChange] = useState(new Date())
   // const [refreshPost, setRefreshPost] = useState('')
 
   const handleEdit = async (data) => {
@@ -42,11 +46,22 @@ const CommunityPosts = () => {
   }, [refreshEdit])
 
   return (
-    <CommunityPostScreen
-      posts={posts}
-      handleEdit={handleEdit}
-      setRefreshEdit={setRefreshEdit}
-    />
+    <div>
+      <div className="row">
+        <div className="col-xs-12 col-lg-9 animate__animated animate__fadeInLeft">
+          <CommunityPostScreen
+            posts={posts}
+            handleEdit={handleEdit}
+            setRefreshEdit={setRefreshEdit}
+          />
+        </div>
+        <div className="col-xs-12 col-lg-3 animate__animated animate__fadeInRight">
+          {/* <Weather community={community} /> */}
+          <Calendar onChange={onChange} value={value} />
+          <Messaging />
+        </div>
+      </div>
+    </div>
   )
 }
 
