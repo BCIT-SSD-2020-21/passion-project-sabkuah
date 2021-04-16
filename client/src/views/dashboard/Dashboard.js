ae7326f96f3ca75920b0a1969c6da728510e56c0
Sabrina Kuah <swkuah@gmail.com>
2021-04-16 11:32:18 -0700

    Update Search screen dashboard widgets, #110

--- client/src/views/dashboard/Dashboard.js
+++ client/src/views/dashboard/Dashboard.js
@@ -10,17 +10,19 @@ const Dashboard = ({ children }) => {
   const [value, onChange] = useState(new Date());
   const location = useLocation();
   const USER_COMMUNITY_URL = '/user/communities';
+  const USER_SEARCH_URL = '/user/communities/search';
   const [token] = useLocalStorage('token');
   const [community, setCommunity] = useState(null);
 
   useEffect(() => {
     const communityId = location.pathname.substr(18, 35);
-    // console.log('community ID >>', communityId);
+    console.log('community ID >>', communityId);
     // console.log('location', location);
     communityId &&
+      communityId !== 'search' &&
       (async () => {
         const response = await getCommunity({ id: communityId, token });
-        console.log('community response', response.community);
+        console.log('community response', response?.community);
         setCommunity(response.community);
       })();
   }, []);
@@ -34,13 +36,15 @@ const Dashboard = ({ children }) => {
 
         <div className='col-xs-12 col-lg-3 col-xs-12 col-lg-3 animate__animated animate__fadeInRight '>
           <div className='d-flex justify-content-center flex-column'>
-            {location.pathname !== USER_COMMUNITY_URL && (
-              <Weather community={community} />
-            )}
+            {location.pathname !== USER_COMMUNITY_URL &&
+              location.pathname !== USER_SEARCH_URL && (
+                <Weather community={community} />
+              )}
             <Calendar onChange={onChange} value={value} />
-            {location.pathname !== USER_COMMUNITY_URL && (
-              <Messaging community={community} />
-            )}
+            {location.pathname !== USER_COMMUNITY_URL &&
+              location.pathname !== USER_SEARCH_URL && (
+                <Messaging community={community} />
+              )}
           </div>
         </div>
       </div>
